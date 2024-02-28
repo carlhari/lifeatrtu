b45aa8bf3ef942a83516171dd7f04c79db3f3a64
Carl Hari Alisangco <alisangcocarl@gmail.com>
2024-02-28 19:40:28 +0800

    chage add post icon

--- src/app/components/overlays/AddPost.tsx
+++ src/app/components/overlays/AddPost.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useAddPost } from "@/utils/useAddPost";
 import React from "react";
-import { BsChatLeftQuote } from "react-icons/bs";
+import { BiMessageDetail } from "react-icons/bi";
 
 function AddPost() {
   const { clicked, click } = useAddPost();
@@ -11,9 +11,9 @@ function AddPost() {
         <button
           type="button"
           onClick={clicked}
-          className=" text-7xl 2xl:text-6xl lg:text-7xl xs:text-5xl text-white animate-bounce fixed bottom-12 right-16 z-40 rounded-full bg-blue-600 border border-solid border-black p-4 2xl:right-8 xs:right-4"
+          className=" text-7xl 2xl:text-6xl lg:text-7xl xs:text-5xl animate-bounce fixed bottom-12 right-16 z-40 p-4 2xl:right-8 xs:right-4"
         >
-          <BsChatLeftQuote />
+          <BiMessageDetail />
         </button>
       </div>
     )
